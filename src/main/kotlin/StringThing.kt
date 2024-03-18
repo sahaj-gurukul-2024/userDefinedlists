4973543cba854a8b5a8e8ad3ce853389a8f6bd0e
Siddhant Jagdish Verma <siddhantjagdishv@Siddhants-MacBook-Pro.local>
2024-03-18 21:29:18 +0530

    Look better ig ocd thing

--- src/main/kotlin/StringThing.kt
+++ src/main/kotlin/StringThing.kt
@@ -26,33 +26,46 @@ sealed interface StringThing {
 }
 
 data class NonEmptyStringThing(val head: String, val tail: StringThing) : StringThing{
-    override fun append(string: String): StringThing = NonEmptyStringThing(head, tail.append(string))
+    override fun append(string: String): StringThing =
+        NonEmptyStringThing(head, tail.append(string))
 
-    override fun prepend(string: String): StringThing = NonEmptyStringThing(string, this)
+    override fun prepend(string: String): StringThing =
+        NonEmptyStringThing(string, this)
 
-    override fun get(index: Int): String = if (index == 0) head else tail.get(index - 1)
+    override fun get(index: Int): String =
+        if (index == 0) head else tail.get(index - 1)
 
-    override fun transform(transformFunction: (String) -> String): StringThing = NonEmptyStringThing(transformFunction(head), tail.transform(transformFunction))
+    override fun transform(transformFunction: (String) -> String): StringThing =
+        NonEmptyStringThing(transformFunction(head), tail.transform(transformFunction))
 
-    override fun intList(): IntegerListThing = NonEmptyIntegerList(head.length, tail.intList())
+    override fun intList(): IntegerListThing =
+        NonEmptyIntegerList(head.length, tail.intList())
 
-    override fun threes(): StringThing = if (head.length == 3) NonEmptyStringThing(head, tail.threes()) else tail.threes()
+    override fun threes(): StringThing =
+        if (head.length == 3) NonEmptyStringThing(head, tail.threes()) else tail.threes()
 
-    override fun processStrings(sep: String,processFunction: (String) -> String): String = "${processFunction(head)}$sep${tail.processStrings(sep,processFunction)}".trim()
+    override fun processStrings(sep: String,processFunction: (String) -> String): String =
+        "${processFunction(head)}$sep${tail.processStrings(sep,processFunction)}".trim()
 }
 
 data object EmptyStringThing : StringThing{
-    override fun append(string: String): StringThing = NonEmptyStringThing(string, EmptyStringThing)
+    override fun append(string: String): StringThing =
+        NonEmptyStringThing(string, EmptyStringThing)
 
-    override fun prepend(string: String): StringThing = NonEmptyStringThing(string, EmptyStringThing)
+    override fun prepend(string: String): StringThing =
+        NonEmptyStringThing(string, EmptyStringThing)
 
-    override fun get(index: Int) = throw IndexOutOfBoundsException()
+    override fun get(index: Int) =
+        throw IndexOutOfBoundsException()
 
-    override fun transform(transformFunction: (String) -> String): StringThing = this
+    override fun transform(transformFunction: (String) -> String): StringThing =
+        EmptyStringThing
 
-    override fun intList(): IntegerListThing = EmptyIntegerList
+    override fun intList(): IntegerListThing =
+        EmptyIntegerList
 
-    override fun threes(): StringThing = EmptyStringThing
+    override fun threes(): StringThing =
+        EmptyStringThing
 
     override fun processStrings(sep: String,processFunction: (String) -> String): String = ""
 }
