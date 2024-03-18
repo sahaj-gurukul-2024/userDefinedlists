d4304558ffdc8d5a7bf1f1cc54d929e1e462beca
Siddhant Jagdish Verma <siddhantjagdishv@Siddhants-MacBook-Pro.local>
2024-03-18 21:35:47 +0530

    Integer List Cleanup

--- src/main/kotlin/ListThing.kt
+++ src/main/kotlin/ListThing.kt
@@ -3,61 +3,27 @@ package org.sahaj.meetingbookingss
 import kotlin.math.pow
 
 sealed interface IntegerListThing {
-    fun append(value: Int): IntegerListThing {
-        return when (this) {
-            EmptyIntegerList -> NonEmptyIntegerList(value, EmptyIntegerList)
-            is NonEmptyIntegerList -> NonEmptyIntegerList(head, tail.append(value))
-        }
-    }
+    fun append(value: Int): IntegerListThing
 
-    fun prepend(value: Int): IntegerListThing {
-        return when (this) {
-            EmptyIntegerList -> NonEmptyIntegerList(value, EmptyIntegerList)
-            is NonEmptyIntegerList -> NonEmptyIntegerList(value, this)
-        }
-    }
+    fun prepend(value: Int): IntegerListThing
 
-    fun getByIndex(index: Int): Int? {
-        return when (this) {
-            EmptyIntegerList -> throw IndexOutOfBoundsException()
-            is NonEmptyIntegerList -> if (index == 0) head else tail.getByIndex(index - 1)
-        }
-    }
+    fun getByIndex(index: Int): Int
 
     fun getPower(power: Int): IntegerListThing = operatorTransform(power) { it.toDouble().pow(power).toInt() }
 
     fun incrementBy(value: Int): IntegerListThing = operatorTransform(value) { it.plus(value) }
 
-    fun operatorTransform(value: Int, transformFunction: (Int) -> Int): IntegerListThing = when (this) {
-        is NonEmptyIntegerList -> NonEmptyIntegerList(
-            transformFunction(head), tail.operatorTransform(value, transformFunction)
-        )
+    fun operatorTransform(value: Int, transformFunction: (Int) -> Int): IntegerListThing
 
-        EmptyIntegerList -> EmptyIntegerList
-    }
-
-    fun sum(): Int? {
-        return when (this) {
-            EmptyIntegerList -> null
-            is NonEmptyIntegerList -> head + (tail.sum() ?: 0)
-        }
-    }
+    fun sum(): Int?
 
     fun odds(): IntegerListThing = filterOddEven { it % 2 != 0 }
 
     fun evens(): IntegerListThing = filterOddEven { it % 2 == 0 }
 
-    fun filterOddEven(filterFunction: (Int) -> Boolean): IntegerListThing = when (this) {
-        EmptyIntegerList -> EmptyIntegerList
-        is NonEmptyIntegerList -> if (filterFunction(head)) NonEmptyIntegerList(
-            head, tail.filterOddEven(filterFunction)
-        ) else tail.filterOddEven(
-            filterFunction
-        )
-    }
+    fun filterOddEven(filterFunction: (Int) -> Boolean): IntegerListThing
 
     fun max(): Int? {
-        //val find: (ListThing) -> (Int) = {}
         return when (this) {
             EmptyIntegerList -> null
             is NonEmptyIntegerList -> {
@@ -73,6 +39,38 @@ sealed interface IntegerListThing {
     }
 }
 
-data class NonEmptyIntegerList(val head: Int, val tail: IntegerListThing) : IntegerListThing
+data class NonEmptyIntegerList(val head: Int, val tail: IntegerListThing) : IntegerListThing{
+    override fun append(value: Int): IntegerListThing = NonEmptyIntegerList(head, tail.append(value))
 
-data object EmptyIntegerList : IntegerListThing
+    override fun prepend(value: Int): IntegerListThing = NonEmptyIntegerList(value, this)
+
+    override fun getByIndex(index: Int): Int = if (index == 0) head else tail.getByIndex(index - 1)
+
+    override fun operatorTransform(value: Int, transformFunction: (Int) -> Int): IntegerListThing =  NonEmptyIntegerList(
+        transformFunction(head), tail.operatorTransform(value, transformFunction)
+    )
+
+    override fun sum(): Int = head + (tail.sum() ?: 0)
+
+    override fun filterOddEven(filterFunction: (Int) -> Boolean): IntegerListThing =
+        if (filterFunction(head)) NonEmptyIntegerList(
+            head, tail.filterOddEven(filterFunction)
+        ) else tail.filterOddEven(
+            filterFunction
+        )
+
+}
+
+data object EmptyIntegerList : IntegerListThing{
+    override fun append(value: Int): IntegerListThing = NonEmptyIntegerList(value, EmptyIntegerList)
+
+    override fun prepend(value: Int): IntegerListThing = NonEmptyIntegerList(value, EmptyIntegerList)
+
+    override fun getByIndex(index: Int): Int =  throw IndexOutOfBoundsException()
+
+    override fun operatorTransform(value: Int, transformFunction: (Int) -> Int): IntegerListThing = EmptyIntegerList
+
+    override fun sum(): Int? = null
+
+    override fun filterOddEven(filterFunction: (Int) -> Boolean): IntegerListThing = EmptyIntegerList
+}
